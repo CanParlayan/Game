8f305855df07fea30f080f00facf2a504339a21b
Halil Can Parlayan <69989315+CanParlayan@users.noreply.github.com>
2022-08-10 15:50:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,"")));
 
 const botName = "Hanoi LobbyBot";
 
