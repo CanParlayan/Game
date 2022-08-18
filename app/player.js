212f47e9e3002bab34287a86e6d15887ded3a8a4
Halil Can Parlayan <69989315+CanParlayan@users.noreply.github.com>
2022-08-18 16:26:24 +0300

    .

---
+++ app/player.js
@@ -0,0 +1,48 @@
+
+/**
+ * Player constructor
+ * @param {type} id Socket ID
+ */
+function Player(id) {
+  var i;
+
+  this.id = id;
+}
+
+/**
+ * Fire shot on grid
+ * @param {type} gridIndex
+ * @returns {Boolean} True if hit
+ */
+
+/**
+ * Get an array of sunk ships
+ * @returns {undefined}
+ */
+
+/**
+ * Get the number of ships left
+ * @returns {Number} Number of ships left
+ */
+
+/**
+ * Create ships and place them randomly in grid
+ * @returns {Boolean}
+ */
+
+/**
+ * Try to place a ship randomly in grid without overlapping another ship.
+ * @param {Ship} ship
+ * @param {Number} shipIndex
+ * @returns {Boolean}
+ */
+
+/**
+ * Check if a ship overlaps another ship in the grid.
+
+/**
+ * Check if there are ships adjacent to this ship placement
+ * @param {Ship} ship
+ * @returns {Boolean} True if adjacent ship found
+ */
+module.exports = Player;
